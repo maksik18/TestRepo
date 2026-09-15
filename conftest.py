b7c73eb44b3c218f629b6fd5e55de3885e914da6
Max <oyi.21.11.00058@Maks.local>
2026-09-15 20:06:46 +0300

    Configure Chrome for CI

--- conftest.py
+++ conftest.py
@@ -19,7 +19,12 @@ settings = Settings()
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
     driver.get(settings.base_url)
 
     yield driver
